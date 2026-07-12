652eedda8878f6d385f52a216cf04342fe8b0d03
nishasingh-cmd <nishasingh59198@gmail.com>
2026-07-12 20:24:00 +0530

    added new video for hero page

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -34,7 +34,7 @@ const Hero = () => {
           playsInline
           className="bg-video"
         >
-          <source src="/EliteEventureVideo.mp4" type="video/mp4" />
+          <source src="/eliteEventureVideo.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
